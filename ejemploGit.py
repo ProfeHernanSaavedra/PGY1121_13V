6395e9cdbc7068054efec75895831f486ca76d96
DOCENTE <docente@LC061500.local>
2022-05-31 22:00:29 -0400

    se verifica edad

--- ejemploGit.py
+++ ejemploGit.py
@@ -1,4 +1,9 @@
 print("Hola mundo")
 nom = input("Ingrese su nombre: ")
 print("Hola ",nom)
+edad = int(input("Ingrese edad: "))
+if edad > 18:
+    print("UD es mayor de edad")
+else:
+    print("UD es un niño(a)")
 
