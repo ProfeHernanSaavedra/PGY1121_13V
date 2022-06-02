9dec9fc3256a8c52df4133c9620804a0687ff940
DOCENTE <docente@LC061500.local>
2022-06-01 21:15:40 -0400

    se agregaron funciones

---
+++ funciones.py
@@ -0,0 +1,11 @@
+def sumar(a,b):
+  suma = a + b
+  return suma
+
+#fin funcion
+
+num1 = int(input("Ingrese numero 1: "))
+num2 = int(input("Ingrese numero 2: "))
+
+sum = sumar(num1,num2)
+print(sum)
